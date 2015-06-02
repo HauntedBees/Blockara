7f71f1908fc1ffabf43c2af0569c7f3ccac21011
Sean Finch <fench@hauntedbees.com>
2015-06-01 22:00:42 -0700

    made white say shit on the title screen
    he ain't got 83 voice clips!

--- Assets/Resources/Scripts/StateControllers/MainMenuController.cs
+++ Assets/Resources/Scripts/StateControllers/MainMenuController.cs
@@ -56,6 +56,7 @@ public class MainMenuController:MenuController {
 			} else {
 				GetGameObject(new Vector3(xOffset, -0.5f), "A Player Is You", sheet[(PD.p2Char == PersistData.C.White)?0:1], false, "Zapper");
 				PD.sounds.SetMusicAndPlay(SoundPaths.M_Title_DerivPath + "White");
+				PD.sounds.SetVoiceAndPlay(SoundPaths.VoicePath + "White/032", 0);
 			}
 		} else {
 			PD.sounds.SetVoiceAndPlay(SoundPaths.NarratorPath + "001", 0);
