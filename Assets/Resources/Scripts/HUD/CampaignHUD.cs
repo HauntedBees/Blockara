b559a92172f0a16ae4b672038eed562171236429
Sean Finch <fench@hauntedbees.com>
2015-06-01 18:46:24 -0700

    added damage reference to endless mode HUD.
    KSimps noticed this~

--- Assets/Resources/Scripts/HUD/CampaignHUD.cs
+++ Assets/Resources/Scripts/HUD/CampaignHUD.cs
@@ -22,6 +22,7 @@ public class CampaignHUD:InGameHUD {
 		GetMeshText(new Vector3(x, 1.03f), GetXmlValue(top, "money"), new FontData(TextAnchor.MiddleLeft, TextAlignment.Left, 0.03f));
 		moneyText = GetMeshText(new Vector3(x + 0.6f, 0.84f), money.ToString(), new FontData(TextAnchor.MiddleRight, TextAlignment.Right, 0.045f));
 		shopText = GetMeshText(new Vector3(x + 0.3f, 0.5f), "", new FontData(TextAnchor.MiddleCenter, TextAlignment.Center, 0.023f));
+		AddDamageReferenceKey();
 	}
 	override public void DoUpdate(bool paused, int p1val, int p2val, bool hiddenPause = false) {
 		if(gameEnd) { return; }
@@ -29,6 +30,11 @@ public class CampaignHUD:InGameHUD {
 		if(!hiddenPause && Time.time >= lastCheck + 1.0f) { UpdateTimer(); }
 		if(p1Score_val != p1val) { p1Score_val = p1val; UpdateTextValueAndSize(p1ScoreText, p1Score_val); }
 		if(money != p2val) { SetAndUpdateMoneyTextSize(p2val); }
+	}
+	private void AddDamageReferenceKey() {
+		Vector2 pos = new Vector2(PD.IsLeftAlignedHUD()?-0.9f:0.9f, 0.0f);
+		GameObject helper = GetGameObject(pos, "Damage Reference", Resources.Load<Sprite>(SpritePaths.GuideCircle + (PD.IsColorBlind()?SpritePaths.ColorblindSuffix:"")), false, "Reference");
+		helper.transform.localScale = new Vector2(0.8f, 0.8f);
 	}
 	private void SetAndUpdateMoneyTextSize(int moneyAmount) { money = moneyAmount; UpdateTextValueAndSize(moneyText, money); }
  	public void UpdateShopBox(string text) { shopText.text = text; }
